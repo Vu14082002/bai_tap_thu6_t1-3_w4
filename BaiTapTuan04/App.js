73c334d102e2bb5c197f3aebd0cbdff7ce88f49b
Vu20020814 <nguyenvanvu20020814@gmail.com>
2023-10-04 22:38:29 +0700

    layout04

--- BaiTapTuan04/App.js
+++ BaiTapTuan04/App.js
@@ -5,9 +5,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Layout02 } from "./src/VIEW/Layout02";
 import { Layout03 } from "./src/VIEW/Layout03";
+import { Layout04 } from "./src/VIEW/Layout04";
 
 export default function App() {
-      return <Layout03 />;
+      return <Layout04 />;
 }
 const styles = StyleSheet.create({
       container: {
---
+++ BaiTapTuan04/src/VIEW/Layout04/index.js
@@ -0,0 +1,107 @@
+import { LinearGradient } from "expo-linear-gradient";
+import {
+      Image,
+      Pressable,
+      StyleSheet,
+      Text,
+      TextInput,
+      View,
+} from "react-native";
+import { Button } from "react-native-web";
+
+export const Layout04 = function () {
+      return (
+            <LinearGradient
+                  style={styles.container}
+                  colors={["#C7F4F7", "#D1F4F6", "#E5F4F5", "#00CCF9"]}
+                  start={{ x: 0, y: 0 }}
+                  end={{ x: 0, y: 1 }}
+            >
+                  <View style={[styles.flex3, styles.header]}>
+                        <Text style={styles.headerContent}>CODE</Text>
+                  </View>
+                  <View style={styles.flex3}>
+                        <Text style={styles.bodyContent}>VERIFICATION</Text>
+                        <Text style={styles.bodyContent1}>
+                              Enter ontime password sent on ++849092605798
+                        </Text>
+                        <View
+                              style={{
+                                    flexDirection: "row",
+                                    justifyContent: "center",
+                              }}
+                        >
+                              {Array.from({ length: 6 }).map((_, index) => {
+                                    return <View style={styles.box}></View>;
+                              })}
+                        </View>
+                  </View>
+                  <View style={styles.flex2}>
+                        <Pressable style={styles.btn}>
+                              <Text style={styles.footerContent}>
+                                    VERIFY CODE
+                              </Text>
+                        </Pressable>
+                  </View>
+            </LinearGradient>
+      );
+};
+
+const styles = StyleSheet.create({
+      container: {
+            flex: 1,
+            fontFamily: "Roboto",
+      },
+      flex3: {
+            flex: 3,
+            justifyContent: "space-around",
+            alignItems: "center",
+      },
+      flex2: {
+            flex: 2,
+            justifyContent: "center",
+            alignItems: "center",
+      },
+      // header
+      header: {
+            justifyContent: "center",
+            alignItems: "center",
+      },
+      headerContent: {
+            fontSize: 60,
+            fontWeight: 700,
+            linHeight: 70,
+      },
+      // body
+      bodyContent: {
+            fontSize: 20,
+            fontWeight: 700,
+            lineHeight: 23,
+            textAlign: "center",
+      },
+      bodyContent1: {
+            fontSize: 15,
+            fontWeight: 700,
+            lineHeight: 18,
+            textAlign: "center",
+            width: "80%",
+      },
+      box: {
+            width: 50,
+            height: 50,
+            border: "1px solid black",
+      },
+      // footer
+      footerContent: {
+            fontSize: 18,
+            fontWeight: 700,
+            lineHeight: 21,
+            textAlign: "center",
+      },
+      btn: {
+            backgroundColor: "#E3C000",
+            justifyContent: "center",
+            width: "95%",
+            height: 50,
+      },
+});
